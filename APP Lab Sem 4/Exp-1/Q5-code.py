2a66a8557517d1d364b62ffef0219c42fcee170a
legendofphoenix <82327479+legendofphoenix@users.noreply.github.com>
2022-02-16 01:09:35 +0530

    Create Q5-code.py

---
+++ APP Lab Sem 4/Exp-1/Q5-code.py
@@ -0,0 +1,14 @@
+def add(x,y):
+    print("x+y=",x+y)
+def sub(x,y):
+    print("x-y=",x-y)
+def mul(x,y):
+    print("x*y",x*y)
+def div(x,y):
+    print("x/y=",x//y)
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+add(x,y)
+sub(x,y)
+mul(x,y)
+div(x,y)
