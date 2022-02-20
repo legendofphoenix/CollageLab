0a22d29d8074ad923026a44b435a7f7068b73e7f
legendofphoenix <82327479+legendofphoenix@users.noreply.github.com>
2022-02-20 23:40:15 +0530

    Create Q5-code.py

---
+++ APP Lab Sem 4/Exp-4/Q5-code.py
@@ -0,0 +1,11 @@
+#Question 1 set9 week 3
+class fruits:
+    number=1
+    def __init__(self,name):
+        self.name=name
+a=fruits('apple')
+b=fruits('kiwi')
+print(a.number)
+print(b.number)
+print(a.name)
+print(fruits.number)
