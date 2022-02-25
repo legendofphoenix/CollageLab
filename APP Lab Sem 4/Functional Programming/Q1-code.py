81f09ebdd8237bb6164f3e3c691373282266141c
legendofphoenix <82327479+legendofphoenix@users.noreply.github.com>
2022-02-25 23:34:46 +0530

    Create Q1-code.py

---
+++ APP Lab Sem 4/Functional Programming/Q1-code.py
@@ -0,0 +1,9 @@
+""" 1. Calculate the following using Lambda calculus:
+a.	T AND F
+b.	3 * 4
+"""
+y=lambda s: s and False
+print("T AND F = ",y(True)) 
+x = lambda a : a*4
+print("3*4= ",x(3))
+print("\n")
