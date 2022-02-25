36ce235fe912b61643414cd75b3c21b2d0f95c3c
legendofphoenix <82327479+legendofphoenix@users.noreply.github.com>
2022-02-25 23:36:54 +0530

    Update Q2-code.py

--- APP Lab Sem 4/Functional Programming/Q2-code.py
+++ APP Lab Sem 4/Functional Programming/Q2-code.py
@@ -4,9 +4,15 @@ b. Write a lambda function in Python to implement the following lambda expressio
 (λf. λm. (f + m)a)(λx. x2)(b)
 Note: You need to write a nested lambda function for implementing f+m where f takes the square function (which takes argument x) passed as a parameter. The above expression calculates a2+b.
 """
-def multiplier_of(n):
-    def multiplier(number):
-            return number*n
-    return multiplier
-multiplywith5 = multiplier_of(5)
-print(multiplywith5(9))
+#Qn 2a
+feet = lambda m: m*(3.281)
+meters = int(input("Enter the number of meters to be converted: "))
+print("{:0.2f} feet".format(feet(meters)))
+
+
+
+square = lambda x: x**2
+total = lambda f, b: lambda a: f(a)+b
+a = int(input("Enter value for a: "))
+b = int(input("Enter value for b: "))
+print(total(square, b)(a))
