c9fce8aea22c58637839eee977a7329b356a7d6d
legendofphoenix <82327479+legendofphoenix@users.noreply.github.com>
2022-02-25 23:40:30 +0530

    Create Q6-code.py

---
+++ APP Lab Sem 4/Functional Programming/Q6-code.py
@@ -0,0 +1,18 @@
+"""6. Map
+A secret message needs to be sent. Use the map function to encrypt the message using Caesar cipher.
+"""
+
+def encrypt(letter, s):
+    if (letter.isupper()):
+        return chr((ord(letter) + s - 65) % 26 + 65)
+    elif (letter.islower()):
+        return chr((ord(letter) + s - 97) % 26 + 97)
+    else:
+        return letter
+text = input("Enter the word: ")
+s = int(input("Enter the shift: "))
+shift = [s] * len(text)
+result = list(map(encrypt, text, shift))
+print("Encrypted text: ", end="")
+for i in result:
+ print(i, end="")
