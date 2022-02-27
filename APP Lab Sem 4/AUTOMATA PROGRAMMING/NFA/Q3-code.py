e42e17700f06705efabc3e8262f92883e568d872
legendofphoenix <82327479+legendofphoenix@users.noreply.github.com>
2022-02-28 01:25:30 +0530

    Create Q3-code.py

---
+++ APP Lab Sem 4/AUTOMATA PROGRAMMING/NFA/Q3-code.py
@@ -0,0 +1,20 @@
+""" Given NFAs for {}, {ε}, {(ab)n | n  N}, which has regular expression (ab)*. """
+from automata.fa.nfa import NFA
+# NFA which accepts strings that ends with 'ab'
+nfa = NFA(
+    states={'q0', 'q1', 'q2'},
+    input_symbols={'a', 'b'},
+    transitions={
+        'q0': {'a': {'q1'}},
+        'q1': {'b': {'q0', 'q2'}},
+        'q2': {}
+    },
+    initial_state='q0',
+    final_states={'q2'}
+)
+for i in range(1,8):
+    num = input("Enter the string :")
+    if(nfa.accepts_input(num)):
+        print("Accepted")
+    else:
+        print("Rejected")
